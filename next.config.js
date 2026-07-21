f418513831231757d57d6a6a4ca69c9e223e8ae3
ValemiMilk <nicolasimoes@valemilk.com.br>
2026-07-21 14:38:27 -0300

    chore: configure GitHub Pages deployment with static export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
+  output: 'export',
+  basePath: '/Otimizador-de-Rotas',
+  images: {
+    unoptimized: true,
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
